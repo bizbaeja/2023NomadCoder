e2b61c5e8fba1ccf138da58129e790b98e9baa9b
bizbaeja <bjgbjg112@gmail.com>
2023-01-19 08:39:56 +0900

    #3.4 innerHTML

--- vanilla-js/app.js
+++ vanilla-js/app.js
@@ -1,8 +1,27 @@
-const age = 96;
-function calculateKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2;
+// <⚠️ DONT DELETE THIS ⚠️>
+const title = document.querySelector("div.hello:first-child h1");
+
+// <⚠️ /DONT DELETE THIS ⚠️>
+function handleTitleClick() {
+  title.style.color = "blue";
 }
 
-const krAge = calculateKrAge(age);
+function handleMouseEnter() {
+  title.innerHTML = "The mouse is here!";
+}
 
-console.log(krAge);
+function handleMouseLeave() {
+  title.innerHTML = "The mouse is  gone!";
+}
+function handleResize() {
+  title.innerHTML = "You just resized!";
+}
+function handleContextMenu() {
+  title.innerHTML = "You was a right click!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+window.addEventListener("resize", handleResize);
+window.addEventListener("contextmenu", handleContextMenu);
