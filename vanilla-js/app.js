96890ec96d25cc0d4f56f7d583ed7db9274ef325
bizbaeja <bjgbjg112@gmail.com>
2023-01-21 11:10:55 +0900

    #6.32

--- vanilla-js/app.js
+++ vanilla-js/app.js
@@ -1,40 +1,15 @@
-// <⚠️ DONT DELETE THIS ⚠️>
-const title = document.querySelector("div.hello:first-child h1");
+const longinInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
-// <⚠️ /DONT DELETE THIS ⚠️>
-function handleTitleClick() {
-  title.style.color = "blue";
+function onLogiBtnClick(e) {
+  e.prevntDefault();
+  console.log(e);
 }
-
-function handleMouseEnter() {
-  title.innerHTML = "The mouse is here!";
-}
-
-function handleMouseLeave() {
-  title.innerHTML = "The mouse is  gone!";
-}
-function handleResize() {
-  title.innerHTML = "You just resized!";
-  document.body.style.backgroundColor = "tomato";
-}
-function handleContextMenu() {
-  title.innerHTML = "You was a right click!";
+function handleLinkClick(e) {
+  console.log(e);
+  alert("clicked!");
 }
 
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-function handleWindowOnline() {
-  alert("All GOOD");
-}
-function handleWindowCopy() {
-  alert("copier!");
-}
-title.onclick = handleTitleClick;
-title.onmouseenter = handleMouseEnter;
-title.onmouseleave = handleMouseLeave;
-window.onresize = handleResize;
-window.oncontextmenu = handleContextMenu;
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
-window.addEventListener("copy", handleWindowCopy);
+loginForm.addEventListener("submit", onLogiBtnClick);
+link.addEventListener("click", handleLinkClick);
