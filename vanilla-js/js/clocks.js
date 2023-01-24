d240bf8ff139d5effad91793d1dc45e081c73f8b
bizbaeja <bjgbjg112@gmail.com>
2023-01-24 12:09:44 +0900

    getClock

--- vanilla-js/js/clocks.js
+++ vanilla-js/js/clocks.js
@@ -1,7 +1,9 @@
-const clock = document.querySelector("clock");
+const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 //setInterval(호출하려는 함수, 반복할시간)
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
